2eaba74d05b9fe762cad0ba29f3b36297115c5eb
szeedneigh <sidneyjohn1704@gmail.com>
2025-06-07 13:13:44 +0800

    Task 3: Add event model and routes | event model

---
+++ models/Event.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, { timestamps: true });
+
+const Event = mongoose.model('Event', eventSchema);
+
+module.exports = Event; 
